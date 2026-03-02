fa6d1f07380efd0f52b79b830a8552ef69eadd62
fibagithub <105911830+fibagithub@users.noreply.github.com>
2026-03-02 11:29:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/mono-flex-design/',  // ← ЭНД НЭМ! Repo нэр чинь "mono-flex-design" учраас яг ингэж бич (том жижиг үсэг яг таарах ёстой, төгсгөлд '/' заавал бай!)
-
   server: {
     host: "::",
     port: 8080,
